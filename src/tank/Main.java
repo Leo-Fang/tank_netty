9a854e448e5373f6897f0967f0df182c01ecc04e
Leo-Fang <lefang0530@gmail.com>
2020-05-25 20:39:55 +0800

    添加键盘监听事件，在主线程中每隔50ms刷新一次窗口，并执行repaint()方法，它会自动调用paint()方法。

--- src/tank/Main.java
+++ src/tank/Main.java
@@ -2,9 +2,16 @@ package tank;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
 		TankFrame tf = new TankFrame();
 		
+		//每隔50ms刷新一次Frame
+		while(true) {
+			Thread.sleep(50);
+			tf.repaint();
+		}
+		
 	}
+	
 }
--- src/tank/TankFrame.java
+++ src/tank/TankFrame.java
@@ -2,6 +2,8 @@ package tank;
 
 import java.awt.Frame;
 import java.awt.Graphics;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -14,6 +16,10 @@ public class TankFrame extends Frame {
 		setResizable(false);
 		setVisible(true);
 		
+		//添加键盘监听事件
+		this.addKeyListener(new MyKeyListener());
+		
+		//添加窗口监听事件，点击Frame右上角的“×”可以关闭窗口
 		addWindowListener(new WindowAdapter() {
 			@Override
 			public void windowClosing(WindowEvent e) {
@@ -25,8 +31,20 @@ public class TankFrame extends Frame {
 	@Override
 	public void paint(Graphics g) {
 		g.fillRect(x, y, 50, 50);
-		x += 30;
-		y += 30;
+	}
+	
+	class MyKeyListener extends KeyAdapter {
+		//重写keyPressed和keyReleased方法
+		@Override
+		public void keyPressed(KeyEvent e) {	
+			x += 100;
+		}
+
+		@Override
+		public void keyReleased(KeyEvent e) {
+			System.out.println("key released");
+		}
+		
 	}
 	
 }
