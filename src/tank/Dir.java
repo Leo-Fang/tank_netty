55eea2411c7c4451c578b219578d6784f9a4646e
Leo-Fang <lefang0530@gmail.com>
2020-05-25 21:11:11 +0800

    根据四个Boolean值，计算坦克移动方向和速度，自动移动。定义坦克方向的enum，根据按键状态指定坦克方向，确定坦克位移

---
+++ src/tank/Dir.java
@@ -0,0 +1,7 @@
+package tank;
+
+public enum Dir {
+
+	LEFT, UP, RIGHT, DOWN
+	
+}
--- src/tank/TankFrame.java
+++ src/tank/TankFrame.java
@@ -9,7 +9,10 @@ import java.awt.event.WindowEvent;
 
 public class TankFrame extends Frame {
 
-	int x = 200, y = 200;
+	private static final int SPEED = 10;//坦克速度
+	int x = 200, y = 200;//设置起始位置
+	Dir dir = Dir.DOWN;//设置起始的移动方向
+	
 	public TankFrame() {
 		setSize(800, 600);
 		setTitle("Tank War");
@@ -31,20 +34,89 @@ public class TankFrame extends Frame {
 	@Override
 	public void paint(Graphics g) {
 		g.fillRect(x, y, 50, 50);
+		switch(dir) {
+		case LEFT:
+			x -= SPEED;
+			break;
+		case RIGHT:
+			x += SPEED;
+			break;
+		case UP:
+			y -= SPEED;
+			break;
+		case DOWN:
+			y += SPEED;
+			break;
+		default:
+			break;
+		}
 	}
 	
 	class MyKeyListener extends KeyAdapter {
+		
+		//设置boolean值来判断移动方向
+		boolean bL = false;
+		boolean bR = false;
+		boolean bU = false;
+		boolean bD = false;
+		
 		//重写keyPressed和keyReleased方法
 		@Override
 		public void keyPressed(KeyEvent e) {	
-			x += 100;
+			int key = e.getKeyCode();
+			switch(key) {
+			case KeyEvent.VK_LEFT:
+				bL = true;
+				break;
+			case KeyEvent.VK_RIGHT:
+				bR = true;
+				break;
+			case KeyEvent.VK_UP:
+				bU = true;
+				break;
+			case KeyEvent.VK_DOWN:
+				bD = true;
+				break;
+			default:
+				break;
+			}			
+			setMainTankDir();
 		}
+
 
 		@Override
 		public void keyReleased(KeyEvent e) {
-			System.out.println("key released");
+			int key = e.getKeyCode();
+			switch(key) {
+			case KeyEvent.VK_LEFT:
+				bL = false;
+				break;
+			case KeyEvent.VK_RIGHT:
+				bR = false;
+				break;
+			case KeyEvent.VK_UP:
+				bU = false;
+				break;
+			case KeyEvent.VK_DOWN:
+				bD = false;
+				break;
+			default:
+				break;
+			}
+			setMainTankDir();
 		}
-		
+	
+		//根据按键设置移动方向
+		public void setMainTankDir() {
+			if(bL)
+				dir = Dir.LEFT;
+			if(bR)
+				dir = Dir.RIGHT;
+			if(bU)
+				dir = Dir.UP;
+			if(bD)
+				dir = Dir.DOWN;
+		}
 	}
 	
 }
